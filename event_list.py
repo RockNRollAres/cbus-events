82d11cee86c266eeb7f65cd41cf4a1138f982451
JButler <butler.421@gmail.com>
2019-10-22 23:14:54 -0400

    Cleaned up

--- event_list.py
+++ event_list.py
@@ -12,16 +12,11 @@ output_writer = csv.writer(output_file)
 output_writer.writerow(['Date', 'Title', 'Location'])
 
 for event in events:
-	# check = input("\nNext?   ")
-	# if check != 'n':
 	title = event.h3.text
 	date = event.find('div', class_='date').text
 	location = event.find('span', class_='venue-title').a.text
-	# print(f"Date: {date.strip()}")
-	# print(f"Event: {title.strip()}")
-	# print(f"Location: {location.strip()}\n")
 	output_writer.writerow([date.strip(), title.strip(), location.strip()])
-	# else:
-	# 	break
+
+# TO DO: Pull event list from 'https://promowestlive.com/columbus'
 
 output_file.close()
